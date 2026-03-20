f34224dc7ec38d659fdbece18cc038a79b2a14b1
Alex Rock Ancelet <pierstoval@gmail.com>
2026-03-20 11:47:16 +0100

    Have two servers running for webhook and admin

--- src/commands/serve.rs
+++ src/commands/serve.rs
@@ -1,6 +1,6 @@
 use crate::serve::DEFAULT_HOST;
 use crate::serve::DEFAULT_WEBHOOK_PORT;
-use crate::serve::DEFAULT_ADMIN_PATH;
+use crate::serve::DEFAULT_ADMIN_PORT;
 use crate::CommandHandler;
 use clap::arg;
 use clap::ArgMatches;
@@ -14,7 +14,7 @@ pub(crate) fn get_command() -> CommandHandler {
             .about("(alias: serve:webhook) Starts the Webhook HTTP server")
             .arg(arg!(--port <PORT> "The TCP port to listen to").default_value(DEFAULT_WEBHOOK_PORT))
             .arg(arg!(--host <HOST> "The network host to listen to").default_value(DEFAULT_HOST))
-            .arg(arg!(--adminpath <ADMINPATH> "The path prefix for the Admin panel").default_value(DEFAULT_ADMIN_PATH))
+            .arg(arg!(--adminport <ADMINPORT> "The path prefix for the Admin panel").default_value(DEFAULT_ADMIN_PORT))
             .arg(arg!(--admin "Whether to enable the Admin panel"))
         ,
         Box::new(execute),
@@ -25,9 +25,9 @@ fn execute(config_file_value: Option<&str>, args: &ArgMatches) -> Option<ExitCod
     let host: Option<&str> = args.get_one::<String>("host").map(|s| s.as_str());
     let port: Option<&str> = args.get_one::<String>("port").map(|s| s.as_str());
     let admin: bool = args.get_flag("admin");
-    let admin_path: Option<&str> = args.get_one::<String>("adminpath").map(|s| s.as_str());
+    let admin_port: Option<&str> = args.get_one::<String>("adminport").map(|s| s.as_str());
 
-    match crate::serve::serve_webhook(config_file_value, host, port, admin, admin_path) {
+    match crate::serve::spawn_servers(config_file_value, host, port, admin, admin_port) {
         Ok(_) => Some(ExitCode::SUCCESS),
         Err(e) => {
             error!("{}", e);
--- src/serve/mod.rs
+++ src/serve/mod.rs
@@ -3,51 +3,62 @@ use crate::config;
 use crate::config::Webhook;
 use crate::db::get_database_connection;
 use crate::http;
-use actix_web::dev::Service;
+use actix_web::dev::{Server, Service};
 use actix_web::middleware::Logger;
 use actix_web::web;
 use actix_web::App;
 use actix_web::HttpResponse;
 use actix_web::HttpServer;
+use anyhow::Error;
 use futures_util::future::Either;
 use futures_util::FutureExt;
 use rusqlite::Connection;
 use std::future;
-use std::io::Error;
-use std::io::ErrorKind;
 use std::sync::Arc;
 use std::sync::Mutex;
 use tokio::sync::mpsc;
 
 pub(crate) const DEFAULT_WEBHOOK_PORT: &str = "8000";
 pub(crate) const DEFAULT_ADMIN_PORT: &str = "8010";
-pub(crate) const DEFAULT_ADMIN_PATH: &str = "/admin";
 pub(crate) const DEFAULT_HOST: &str = "127.0.0.1";
 pub(crate) const WEBHOOK_API_PATH: &str = "/webhook";
 
 #[actix_web::main]
-pub(crate) async fn serve_webhook(
+pub(crate) async fn spawn_servers(
     config_file: Option<&str>,
     host: Option<&str>,
     port: Option<&str>,
     admin: bool,
-    admin_path: Option<&str>,
-) -> std::io::Result<()> {
+    admin_port: Option<&str>,
+) -> Result<(), anyhow::Error> {
+    let webhook_server = serve_webhook(config_file.clone(), host.clone(), port)?;
+
+    if admin {
+        let admin_server = serve_admin(config_file.clone(), host.clone(), admin_port)?;
+
+        futures::future::try_join(webhook_server, admin_server).await?;
+    } else {
+        webhook_server.await?;
+    }
+
+    Ok(())
+}
+
+pub(crate) fn serve_webhook(
+    config_file: Option<&str>,
+    host: Option<&str>,
+    port: Option<&str>,
+) -> Result<Server, Error> {
     let host = host.unwrap_or(DEFAULT_HOST);
     let port = port.unwrap_or(DEFAULT_WEBHOOK_PORT);
 
     let port_as_int = port.parse::<u16>().expect("Invalid port value.");
 
-    let config = config::get_config(config_file);
+    let config = config::get_config(config_file)?;
 
-    if config.is_err() {
-        return Err(Error::new(ErrorKind::Other, config.unwrap_err()));
-    }
-
-    let config = config.unwrap();
     let database_file = config.database_file.clone();
 
-    let database_connection = get_database_connection(database_file).unwrap();
+    let database_connection = get_database_connection(database_file)?;
 
     let (sender, receiver) = mpsc::channel(8);
 
@@ -60,56 +71,57 @@ pub(crate) async fn serve_webhook(
 
     info!("Starting HTTP server on {}:{}", host, port);
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(config.clone())
-            .app_data(transmitter_data.clone())
-            .wrap(Logger::default())
-            .service(web::resource(WEBHOOK_API_PATH).to(crate::http::webhook::webhook))
-    })
-    .bind((host, port_as_int))?
-    .run()
-    .await
+    Ok(
+        HttpServer::new(move || {
+            App::new()
+                .app_data(config.clone())
+                .app_data(transmitter_data.clone())
+                .wrap(Logger::default())
+                .service(web::resource(WEBHOOK_API_PATH).to(crate::http::webhook::webhook))
+        })
+            .bind((host, port_as_int))?
+            .run()
+    )
 }
 
-#[actix_web::main]
-pub(crate) async fn serve_admin(
+pub(crate) fn serve_admin(
     config_file: Option<&str>,
     host: Option<&str>,
     port: Option<&str>,
-) -> std::io::Result<()> {
+) -> Result<Server, anyhow::Error> {
     let host = host.unwrap_or(DEFAULT_HOST);
     let port = port.unwrap_or(DEFAULT_ADMIN_PORT);
 
     let port_as_int = port.parse::<u16>().expect("Invalid port value.");
 
-    let config = config::get_config(config_file).unwrap();
-    let database_connection = get_database_connection(config.database_file.clone()).unwrap();
+    let config = config::get_config(config_file)?;
+    let database_connection = get_database_connection(config.database_file.clone())?;
 
     let config = web::Data::new(config);
     let database_connection = web::Data::new(Arc::new(Mutex::new(database_connection)));
 
     info!("Starting HTTP server on {}:{}", host, port);
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(config.clone())
-            .app_data(database_connection.clone())
-            // .wrap(http::admin::AdminAsset::new())
-            .wrap_fn(|sreq, srv| {
-                let path = sreq.path().to_string();
-                let asset_exists: Option<HttpResponse> = http::admin::frontend_assets(path.clone());
+    Ok(
+        HttpServer::new(move || {
+            App::new()
+                .app_data(config.clone())
+                .app_data(database_connection.clone())
+                // .wrap(http::admin::AdminAsset::new())
+                .wrap_fn(|sreq, srv| {
+                    let path = sreq.path().to_string();
+                    let asset_exists: Option<HttpResponse> = http::admin::frontend_assets(path.clone());
 
-                match asset_exists {
-                    None => Either::Left(srv.call(sreq).map(|res| res)),
-                    Some(asset) => return Either::Right(future::ready(Ok(sreq.into_response(asset)))),
-                }
-            })
-            .service(http::admin::api_root)
-    })
-    .bind((host, port_as_int))?
-    .run()
-    .await
+                    match asset_exists {
+                        None => Either::Left(srv.call(sreq).map(|res| res)),
+                        Some(asset) => return Either::Right(future::ready(Ok(sreq.into_response(asset)))),
+                    }
+                })
+                .service(http::admin::api_root)
+        })
+            .bind((host, port_as_int))?
+            .run()
+    )
 }
 
 fn start_workers(mut receiver: mpsc::Receiver<Vec<Webhook>>, conn: Connection) {
